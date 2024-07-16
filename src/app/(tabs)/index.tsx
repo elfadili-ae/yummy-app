c3261a302de40b830fd5357850dc1be9f66877d0
EL FADILI Abdessamad <elfadili.ae@gmail.com>
2024-07-16 23:53:18 +0000

    added products flatList

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,41 +1,16 @@
-import { Image, StyleSheet, Text, View } from 'react-native';
+import {FlatList, View } from 'react-native';
 
-import EditScreenInfo from '@/components/EditScreenInfo';
-import Colors from '@/constants/Colors';
 import products from '../../../assets/data/products';
+import { Product } from '@/types';
+import ProductItem from '@/components/ProductItem';
 
-const product = products[0];
-
-export default function TabOneScreen() {
+export default function MenuScreen() {
   return (
-    <View style={styles.container}>
-      <Image source={{uri: product.image}} style={styles.image} />
-      <Text style={styles.title}>{product.name}</Text>
-      <Text style={styles.price}>{product.price}$</Text>
-    </View>
+      <FlatList 
+      data={products} 
+      renderItem={(item) => <ProductItem product={item.item}/>}
+      numColumns={2}
+      contentContainerStyle={{gap: 10, padding: 10}}
+      columnWrapperStyle={{gap: 10}} />
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-
-  },
-  image: {
-    width: '100%',
-    height: 100
-  },
-  title: {
-    fontSize: 20,
-    fontWeight: '600',
-    marginVertical: 10,
-  },
-  price: {
-    fontSize: 16,
-    color: Colors.light.tint
-  },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
-  },
-});
---
+++ src/components/ProductItem.tsx
@@ -0,0 +1,49 @@
+import { Image, Pressable, StyleSheet, Text, View } from 'react-native';
+
+import Colors from '@/constants/Colors';
+import { Product } from '@/types';
+import { Link } from 'expo-router';
+
+type ProductItemProps = {
+  product: Product;
+}
+
+const ProductItem = ({product}: ProductItemProps) => {
+  return <Link href={`/${product.id}`} asChild>
+    <Pressable key={product.id} style={styles.container}>
+    <Image source={{uri: product.image}} style={styles.image} resizeMode='contain' />
+    <Text style={styles.title}>{product.name}</Text>
+    <Text style={styles.price}>{product.price}$</Text>
+    </Pressable>
+  </Link>
+}
+
+export default ProductItem;
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: 'white',
+    padding: 10,
+    borderRadius: 20,
+    flex: 1,
+    width: '50%'
+  },
+  image: {
+    width: '100%',
+    aspectRatio: 1
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: '600',
+    marginVertical: 10,
+  },
+  price: {
+    fontSize: 16,
+    color: Colors.light.tint
+  },
+  separator: {
+    marginVertical: 30,
+    height: 1,
+    width: '80%',
+  },
+});
